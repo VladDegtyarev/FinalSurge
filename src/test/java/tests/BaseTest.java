95a637fa4830c0777913fc5107259b6e159bc609
VladDegtyarev <vladdegtyarev3@gmail.com>
2025-10-07 16:21:26 +0300

    Test Settings

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,15 +5,15 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import listners.TestListener;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.*;
 import pages.CalendarPage;
 import pages.LoginPage;
 import pages.LogoutPage;
 import pages.SettingPage;
 import utils.PropertyReader;
 
+import static com.codeborne.selenide.Configuration.browser;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 @Listeners(TestListener.class)
@@ -26,16 +26,23 @@ public class BaseTest {
     String user = System.getProperty("user", PropertyReader.getProperty("user"));
     String password = System.getProperty("password", PropertyReader.getProperty("password"));
 
-    @BeforeMethod
-    public void setUp() {
-        Configuration.browser = "chrome";
+    @Parameters({"browser"})
+    @BeforeMethod(description = "Настройка браузера", alwaysRun = true)
+    public void setUp(@Optional("chrome") String browser) {
+        if((browser.equalsIgnoreCase("chrome"))){
+            Configuration.browser = "chrome";
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--incognito");
+        }else if((browser.equalsIgnoreCase("firefox"))){
+            Configuration.browser = "firefox";
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--incognito");
+        }
         Configuration.baseUrl = "https://log.finalsurge.com";
         Configuration.timeout = 40000;
         Configuration.clickViaJs = true;
-        Configuration.headless = true;
-        Configuration.browserSize = "1920x1080";
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
+        //Configuration.headless = true;
+        Configuration.browserSize = "1600x900";
         loginPage = new LoginPage();
         settingPage = new SettingPage();
         calendarPage = new CalendarPage();
